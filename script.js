8524d502da16292efd86a8030c0976a5a655f56f
Nooorrrr <noor.bnhdd@gmail.com>
2023-06-30 22:12:07 +0100

    Fixed case rock vs paper

--- script.js
+++ script.js
@@ -23,11 +23,11 @@ function playRound(playerSelection, computerSelection) {
     console.log("It' s a tie no one won");
     return 0;}
     else if (playerSelection==='rock' && computerSelection==='paper'){
-    console.log("computer chose paper\nYou won ");
-    return 1;}
-    else if (playerSelection==='paper' && computerSelection==='rock'){
-    console.log("computer chose rock\nComputer won ");
+    console.log("computer chose paper\nComputer won ");
     return -1;}
+    else if (playerSelection==='paper' && computerSelection==='rock'){
+    console.log("computer chose rock\nYou won ");
+    return 1;}
     else if (playerSelection==='paper' && computerSelection==='scissors'){
     console.log("computer chose scissors\nComputer won ");
     return -1;}
