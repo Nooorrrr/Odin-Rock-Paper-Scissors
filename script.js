1f14ef6e25fa5b8857931b06da137481851a7395
Nooorrrr <noor.bnhdd@gmail.com>
2023-10-18 19:29:00 +0100

    rps ui done

--- script.js
+++ script.js
@@ -1,71 +1,64 @@
 
-function getComputerChoice()
-{
+
+var computerSelection;
+var playerSelection;
+var img = document.createElement("img")
+var img2 = document.createElement("img")
+
+
 var items = ["rock","paper","scissors"];
-return items[Math.floor(Math.random()*items.length)];
-}
-function getPlayerChoice()
-{
-    const PlayerChoice = prompt("Choose rock, paper or scissors :");
-    if (PlayerChoice.toLowerCase() === "rock"){
-        return 'rock';
-    } else if (PlayerChoice.toLowerCase() === "paper"){
-        return 'paper';
-    }else if (PlayerChoice.toLowerCase() === "scissors"){
-        return 'scissors';
-    }else {
-        console.log("ERROR");
-    }
+
+let buttons = document.querySelectorAll(".btn");
+for(let i =0; i<3;i++){
+    buttons[i].addEventListener("click",function(){
+        playerSelection=items[i];
+        img2.src= playerSelection +".png"
+        document.getElementById("you").innerText="" //removes the "you" when picking a choice
+        document.getElementById("you").appendChild(img2);
+        playRound()
+    });
 }
 
-function playRound(playerSelection, computerSelection) {
+var youWon =0;
+var botWon =0;
+var draw=0;
+
+function playRound() {
+
+    computerSelection = items[Math.floor(Math.random()*items.length)];
+    img.src= computerSelection +".png"
+    document.getElementById("bot").innerText=""
+    document.getElementById("bot").appendChild(img);
+
+
     if (playerSelection===computerSelection){
     console.log("It' s a tie no one won");
-    return 0;}
+        draw += 1;
+    }
     else if (playerSelection==='rock' && computerSelection==='paper'){
     console.log("computer chose paper\nComputer won ");
-    return -1;}
+    botWon +=1;
+    }
     else if (playerSelection==='paper' && computerSelection==='rock'){
     console.log("computer chose rock\nYou won ");
-    return 1;}
+    youWon +=1;}
     else if (playerSelection==='paper' && computerSelection==='scissors'){
     console.log("computer chose scissors\nComputer won ");
-    return -1;}
+    botWon +=1;}
     else if (playerSelection==='scissors' && computerSelection==='paper'){
     console.log("computer chose paper\nYou won ")
-    return 1;}
+    youWon+=1;}
     else if (playerSelection==='rock' && computerSelection==='scissors'){
     console.log("computer chose scissors\nYou won ")
-    return 1}
+    youWon +=1;}
     else if (playerSelection==='scissors' && computerSelection==='rock'){
     console.log("computer chose rock\nComputer won ")
-    return -1;}
+    botWon +=1;}
+
+    document.getElementById("youScore").innerText = youWon;
+    document.getElementById("botScore").innerText = botWon;
+    document.getElementById("draw").innerText = draw;
 }
 
 
-function game (){
-    let c=0,p=0;
-for(let i=0;i<5;i++){
-computerSelection=getComputerChoice();
-playerSelection=getPlayerChoice();
-k=playRound(playerSelection, computerSelection);
-switch(k){
-    case 1:
-        p++;
-    break;
-    case 0:
-        c++;
-    break;
-}
-}
-if(p>c){
-    console.log("you won the whole game");
-}else if (c>p){
-    console.log(" computer won the whole game");
-}else {
-    console.log("It's a TIE");
-}
-}
-
-console.log(game());
 
